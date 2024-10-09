0d2d905f4bfe7c23acb6231914ab9adda749083e
Arkadiusz Gałka <arkadiuszmg@gmail.com>
2024-10-09 21:13:05 +0200

    Zadanie 4

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+//Console.WriteLine("Hello, World!");
+
+string name = "Ewa";
+
+char sex = 'M';
+
+var age = 17;
+
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine($"{name}, lat {age}");
+}
+else if (sex != 'F' &&  age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+
+}
