b65f3fda364efdc327ebafc3bbcfe76221f312d2
Arkadiusz Gałka <arkadiuszmg@gmail.com>
2024-10-21 13:42:06 +0200

    Zadanie 5.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,27 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.Serialization;
 
+int nr = 748077714;
+string nrInString = nr.ToString();
+char[] digits = nrInString.ToCharArray();
 
-//Console.WriteLine("Hello, World!");
+Console.WriteLine($"Number: {nr}");
 
-string name = "Ewa";
+int[] numberOfDigit = new int[10];
 
-char sex = 'M';
-
-var age = 17;
-
-if (sex == 'F' && age < 30)
+foreach (char digit in digits)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (digit == '0')
+    {
+        numberOfDigit[0]++;
+    }
+    else if (digit == '1')
+    {
+        numberOfDigit[1]++;
+    }
+    else if (digit == '2')
+    {
+        numberOfDigit[2]++;
+    }
+    else if (digit == '3')
+    {
+        numberOfDigit[3]++;
+    }
+    else if (digit == '4')
+    {
+        numberOfDigit[4]++;
+    }
+    else if (digit == '5')
+    {
+        numberOfDigit[5]++;
+    }
+    else if (digit == '6')
+    {
+        numberOfDigit[6]++;
+    }
+    else if (digit == '7')
+    {
+        numberOfDigit[7]++;
+    }
+    else if (digit == '8')
+    {
+        numberOfDigit[8]++;
+    }
+    else if (digit == '9')
+    {
+        numberOfDigit[9]++;
+    }
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine($"{name}, lat {age}");
-}
-else if (sex != 'F' &&  age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
 
-}
+
+Console.WriteLine($"0 => {numberOfDigit[0]}");
+Console.WriteLine($"1 => {numberOfDigit[1]}");
+Console.WriteLine($"2 => {numberOfDigit[2]}");
+Console.WriteLine($"3 => {numberOfDigit[3]}");
+Console.WriteLine($"4 => {numberOfDigit[4]}");
+Console.WriteLine($"5 => {numberOfDigit[5]}");
+Console.WriteLine($"6 => {numberOfDigit[6]}");
+Console.WriteLine($"7 => {numberOfDigit[7]}");
+Console.WriteLine($"8 => {numberOfDigit[8]}");
+Console.WriteLine($"9 => {numberOfDigit[9]}");
