ea8882db0ede99ec7584ef4e5cc8bc3855552bff
Arkadiusz Gałka <arkadiuszmg@gmail.com>
2024-10-23 21:18:39 +0200

    Zadanie 6.0

---
+++ ConsoleApp/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Employee
+    {
+        private List<int> grades = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+
+        public void AddGrade(int grade)
+        {
+            this.grades.Add(grade);
+        }
+
+        public int TotalGradesForEmployee
+        {
+            get
+            {
+                return this.grades.Sum();
+            }
+        }
+
+    }
+
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,45 @@
 ﻿using ConsoleApp;
 using System.Threading.Tasks.Sources;
 
-User user1 = new User("Antek", "qwerty");
-User user2 = new User("Marek", "qwerty");
-User user3 = new User("Bartek", "qwerty");
-User user4 = new User("Zaneta", "qwerty");
+Employee emp1 = new Employee("Tomasz", "Nowak", 27);
+Employee emp2 = new Employee("Marek", "Kowalski", 33);
+Employee emp3 = new Employee("Łukasz", "Bąk", 30);
 
-user1.AddScore(5);
-user1.AddScore(10);
+emp1.AddGrade(5);
+emp1.AddGrade(7);
+emp1.AddGrade(6);
+emp1.AddGrade(9);
+emp1.AddGrade(6);
 
-var result = user1.Result;
 
-Console.WriteLine(result);
+emp2.AddGrade(4);
+emp2.AddGrade(2);
+emp2.AddGrade(4);
+emp2.AddGrade(10);
+emp2.AddGrade(7);
+
+
+emp3.AddGrade(9);
+emp3.AddGrade(7);
+emp3.AddGrade(6);
+emp3.AddGrade(8);
+emp3.AddGrade(8);
+
+List<Employee> employeeList = new List<Employee>()
+{
+    emp1, emp2, emp3
+};
+
+int maxGrade = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (Employee employee in employeeList)
+{
+    if (employee.TotalGradesForEmployee > maxGrade)
+    {
+        employeeWithMaxResult = employee;
+    }
+}
+
+Console.WriteLine($"The best worker is: {employeeWithMaxResult.name} {employeeWithMaxResult.surname}. With result: {employeeWithMaxResult.TotalGradesForEmployee}");
 
