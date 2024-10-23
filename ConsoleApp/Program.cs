2c0fbbeead5cb2918f19791e6127bc12ab09c704
Arkadiusz Gałka <arkadiuszmg@gmail.com>
2024-10-23 21:15:21 +0200

    6 klasy i metody

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,65 +1,15 @@
-﻿using System.Runtime.Serialization;
+﻿using ConsoleApp;
+using System.Threading.Tasks.Sources;
 
-int nr = 748077714;
-string nrInString = nr.ToString();
-char[] digits = nrInString.ToCharArray();
+User user1 = new User("Antek", "qwerty");
+User user2 = new User("Marek", "qwerty");
+User user3 = new User("Bartek", "qwerty");
+User user4 = new User("Zaneta", "qwerty");
 
-Console.WriteLine($"Number: {nr}");
+user1.AddScore(5);
+user1.AddScore(10);
 
-int[] counterOfDigit = new int[10];
+var result = user1.Result;
 
-foreach (char digit in digits)
-{
-    if (digit == '0')
-    {
-        counterOfDigit[0]++;
-    }
-    else if (digit == '1')
-    {
-        counterOfDigit[1]++;
-    }
-    else if (digit == '2')
-    {
-        counterOfDigit[2]++;
-    }
-    else if (digit == '3')
-    {
-        counterOfDigit[3]++;
-    }
-    else if (digit == '4')
-    {
-        counterOfDigit[4]++;
-    }
-    else if (digit == '5')
-    {
-        counterOfDigit[5]++;
-    }
-    else if (digit == '6')
-    {
-        counterOfDigit[6]++;
-    }
-    else if (digit == '7')
-    {
-        counterOfDigit[7]++;
-    }
-    else if (digit == '8')
-    {
-        counterOfDigit[8]++;
-    }
-    else if (digit == '9')
-    {
-        counterOfDigit[9]++;
-    }
-}
+Console.WriteLine(result);
 
-
-Console.WriteLine($"0 => {counterOfDigit[0]}");
-Console.WriteLine($"1 => {counterOfDigit[1]}");
-Console.WriteLine($"2 => {counterOfDigit[2]}");
-Console.WriteLine($"3 => {counterOfDigit[3]}");
-Console.WriteLine($"4 => {counterOfDigit[4]}");
-Console.WriteLine($"5 => {counterOfDigit[5]}");
-Console.WriteLine($"6 => {counterOfDigit[6]}");
-Console.WriteLine($"7 => {counterOfDigit[7]}");
-Console.WriteLine($"8 => {counterOfDigit[8]}");
-Console.WriteLine($"9 => {counterOfDigit[9]}");
---
+++ ConsoleApp/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class User
+    {
+        private List<int> score = new List<int>();
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+    }
+}
