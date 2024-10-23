2ba1a70665dc0edc76e069ded421c66118e2eba7
Arkadiusz Gałka <arkadiuszmg@gmail.com>
2024-10-23 18:45:02 +0200

    Zadanie 5.1 - wykorzystanie tablicy

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,60 +6,60 @@ char[] digits = nrInString.ToCharArray();
 
 Console.WriteLine($"Number: {nr}");
 
-int[] numberOfDigit = new int[10];
+int[] counterOfDigit = new int[10];
 
 foreach (char digit in digits)
 {
     if (digit == '0')
     {
-        numberOfDigit[0]++;
+        counterOfDigit[0]++;
     }
     else if (digit == '1')
     {
-        numberOfDigit[1]++;
+        counterOfDigit[1]++;
     }
     else if (digit == '2')
     {
-        numberOfDigit[2]++;
+        counterOfDigit[2]++;
     }
     else if (digit == '3')
     {
-        numberOfDigit[3]++;
+        counterOfDigit[3]++;
     }
     else if (digit == '4')
     {
-        numberOfDigit[4]++;
+        counterOfDigit[4]++;
     }
     else if (digit == '5')
     {
-        numberOfDigit[5]++;
+        counterOfDigit[5]++;
     }
     else if (digit == '6')
     {
-        numberOfDigit[6]++;
+        counterOfDigit[6]++;
     }
     else if (digit == '7')
     {
-        numberOfDigit[7]++;
+        counterOfDigit[7]++;
     }
     else if (digit == '8')
     {
-        numberOfDigit[8]++;
+        counterOfDigit[8]++;
     }
     else if (digit == '9')
     {
-        numberOfDigit[9]++;
+        counterOfDigit[9]++;
     }
 }
 
 
-Console.WriteLine($"0 => {numberOfDigit[0]}");
-Console.WriteLine($"1 => {numberOfDigit[1]}");
-Console.WriteLine($"2 => {numberOfDigit[2]}");
-Console.WriteLine($"3 => {numberOfDigit[3]}");
-Console.WriteLine($"4 => {numberOfDigit[4]}");
-Console.WriteLine($"5 => {numberOfDigit[5]}");
-Console.WriteLine($"6 => {numberOfDigit[6]}");
-Console.WriteLine($"7 => {numberOfDigit[7]}");
-Console.WriteLine($"8 => {numberOfDigit[8]}");
-Console.WriteLine($"9 => {numberOfDigit[9]}");
+Console.WriteLine($"0 => {counterOfDigit[0]}");
+Console.WriteLine($"1 => {counterOfDigit[1]}");
+Console.WriteLine($"2 => {counterOfDigit[2]}");
+Console.WriteLine($"3 => {counterOfDigit[3]}");
+Console.WriteLine($"4 => {counterOfDigit[4]}");
+Console.WriteLine($"5 => {counterOfDigit[5]}");
+Console.WriteLine($"6 => {counterOfDigit[6]}");
+Console.WriteLine($"7 => {counterOfDigit[7]}");
+Console.WriteLine($"8 => {counterOfDigit[8]}");
+Console.WriteLine($"9 => {counterOfDigit[9]}");
